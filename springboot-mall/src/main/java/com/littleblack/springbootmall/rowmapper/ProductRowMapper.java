c2550982d33be24a74f6bdf4a49e7e76df1655c9
littleblackmann <b0986816338@gmail.com>
2024-09-10 19:42:27 +0800

    商品功能 - Refactor category , 使用 Enum 類型

--- springboot-mall/src/main/java/com/littleblack/springbootmall/rowmapper/ProductRowMapper.java
+++ springboot-mall/src/main/java/com/littleblack/springbootmall/rowmapper/ProductRowMapper.java
@@ -1,5 +1,6 @@
 package com.littleblack.springbootmall.rowmapper;
 
+import com.littleblack.springbootmall.constant.ProductCategory;
 import com.littleblack.springbootmall.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -13,13 +14,25 @@ public class ProductRowMapper implements RowMapper<Product> {
         Product product = new Product();
 
         product.setProductId(resultSet.getInt("product_id"));
+
         product.setProductName(resultSet.getString("product_name"));
-        product.setCategory(resultSet.getString("category"));
+
+        String categoryStr = resultSet.getString("category");
+        ProductCategory category = ProductCategory.valueOf(categoryStr);
+        product.setCategory(category);
+//        product.setCategory(ProductCategory.valueOf(resultSet.getString("category")));
+
+
         product.setImageUrl(resultSet.getString("image_url"));
+
         product.setPrice(resultSet.getInt("price"));
+
         product.setStock(resultSet.getInt("stock"));
+
         product.setDescription(resultSet.getString("description"));
+
         product.setCreatedDate(resultSet.getDate("created_date"));
+
         product.setLastModifiedDate(resultSet.getDate("last_modified_date"));
 
         return product;
